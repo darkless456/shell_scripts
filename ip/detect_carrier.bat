bcccad6f518c404389c44b2c7dcacff5caad0bc9
darkless456 <narcissu456@gmail.com>
2025-04-17 22:36:11 +0800

    boot proxy by network carrier

--- ip/detect_carrier.bat
+++ ip/detect_carrier.bat
@@ -1,39 +1,69 @@
 @echo off
 setlocal enabledelayedexpansion
 
-REM 获取公网IP地址
-for /f "delims=" %%i in ('powershell -c "(Invoke-WebRequest -Uri 'https://api.ipify.org' -TimeoutSec 5).Content.Trim()"') do set "ip=%%i"
-if "%ip%"=="" (
-    echo 无法获取IP地址
-    exit /b 1
+:: Define log file path
+set logfile=D:\ProxyTools\sing-box\bootProxy.log
+
+:: Function to get current timestamp
+for /f "tokens=1-4 delims=:.," %%a in ("%time%") do (
+    set hour=%%a
+    set minute=%%b
+    set second=%%c
+    set centisecond=%%d
+)
+set timestamp=%date% %hour%:%minute%:%second%.%centisecond%
+
+:: Log the start of the script
+echo [%timestamp%] Script started >> %logfile%
+
+:: Get the IP address of the current network interface
+for /f "tokens=*" %%o in ('powershell -c "(Invoke-RestMethod -Uri 'http://ip-api.com/json/%ip%?fields=org').org"') do (
+    set "operator=%%o"
+    set timestamp=%date% %time%
+    echo [%timestamp%] Operator detected: !operator! >> %logfile%
 )
 
-REM 使用国内接口查询运营商（GBK编码）
-for /f "tokens=*" %%o in ('powershell -c "$response = Invoke-WebRequest -Uri 'http://whois.pconline.com.cn/ipJson.jsp?ip=%ip%' -TimeoutSec 5; $gbk = [System.Text.Encoding]::GetEncoding('GBK'); $json = $gbk.GetString($response.Content); ($json | ConvertFrom-Json).addr.split()[-1]"') do set "operator=%%o"
-
-REM 判断运营商并显示结果
-if "%operator%"=="电信" (
-    echo 中国电信
-) else if "%operator%"=="移动" (
-    echo 中国移动
-) else if "%operator%"=="联通" (
-    echo 中国联通
+if not "%operator:Mobile=%"=="%operator%" (
+    set timestamp=%date% %time%
+    echo [%timestamp%] Mobile >> %logfile%
+    set configFile=config.hk.jsonc
+) else if not "%operator:Telecom=%"=="%operator%" (
+    set timestamp=%date% %time%
+    echo [%timestamp%] Telecom >> %logfile%
+    set configFile=config.us.jsonc
+) else if not "%operator:Unicom=%"=="%operator%" (
+    set timestamp=%date% %time%
+    echo [%timestamp%] Unicom >> %logfile%
+    set configFile=config.us.jsonc
 ) else (
-    echo 其他运营商
+    set timestamp=%date% %time%
+    echo [%timestamp%] Other >> %logfile%
+    set configFile=config.us.jsonc
 )
 
-REM 国际接口方案（替换查询部分）
-@REM for /f "tokens=*" %%o in ('powershell -c "(Invoke-RestMethod -Uri 'http://ip-api.com/json/%ip%?fields=org').org"') do set "operator=%%o"
+echo [%timestamp%] Using config file: %configFile% >> %logfile%
 
-@REM if not "%operator:Mobile=%"=="%operator%" (
-@REM     echo 中国移动
-@REM ) else if not "%operator:Telecom=%"=="%operator%" (
-@REM     echo 中国电信
-@REM ) else if not "%operator:Unicom=%"=="%operator%" (
-@REM     echo 中国联通
-@REM ) else (
-@REM     echo 其他运营商
-@REM )
+:: Check if 'sing-box' is running
+tasklist /FI "IMAGENAME eq sing-box.exe" 2>NUL | find /I /N "sing-box.exe">NUL
+if "%ERRORLEVEL%"=="0" (
+    set timestamp=%date% %time%
+    echo [%timestamp%] 'sing-box' is running, closing it... >> %logfile%
+    taskkill /F /IM sing-box.exe >> %logfile%
+    set timestamp=%date% %time%
+    echo [%timestamp%] Rebooting 'sing-box'... >> %logfile%
+    @REM start "" "d:\path\to\sing-box.exe" >> %logfile%  :: Correct this path
+) else (
+    set timestamp=%date% %time%
+    echo [%timestamp%] 'sing-box' is not running, booting it... >> %logfile%
+    @REM start "" "d:\path\to\sing-box.exe" >> %logfile%  :: Correct this path
+)
+
+rem D:
+rem cd D:\ProxyTools\sing-box
+start D:\ProxyTools\sing-box\RunHiddenConsole.exe D:\ProxyTools\sing-box\sing-box.exe run -c %configFile%"
+
+:: Log the end of the script
+set timestamp=%date% %time%
+echo [%timestamp%] Script ended >> %logfile%
 
 endlocal
-
