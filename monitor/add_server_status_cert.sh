cc5541f709b61d0e7be3cf6093cc7f1d8a677e1b
darkless456 <narcissu456@gmail.com>
2025-04-28 23:42:00 +0800

    ipv6 support for server status

--- monitor/add_server_status_cert.sh
+++ monitor/add_server_status_cert.sh
@@ -2,6 +2,7 @@
 
 
 echo "144.34.226.109  server.self-media.org" >> /etc/hosts
+# echo "2607:8700:5500:5b99::2  server.self-media.org" >> /etc/hosts
 
 echo -n | openssl s_client -showcerts -connect server.self-media.org:443 2>/dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > self-media.org.crt
 
