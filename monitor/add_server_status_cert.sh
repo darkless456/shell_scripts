88d2571e9674a6b5ec42c2aab72c9a4e8ca56f90
darkless456 <narcissu456@gmail.com>
2025-05-21 00:11:27 +0800

    script fix

--- monitor/add_server_status_cert.sh
+++ monitor/add_server_status_cert.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-# echo "144.34.226.109  server.self-media.org" >> /etc/hosts
-echo "2607:8700:5500:5b99::2  server.self-media.org" >> /etc/hosts
+echo "144.34.226.109  server.self-media.org" >> /etc/hosts
+# echo "2607:8700:5500:5b99::2  server.self-media.org" >> /etc/hosts
 
 echo -n | openssl s_client -showcerts -connect server.self-media.org:443 2>/dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > self-media.org.crt
 
